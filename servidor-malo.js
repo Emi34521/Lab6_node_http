c55d1c14cb77c81d2e567d89135a7cf6331b309d
Emi34521 <cumatzemilio6@gmail.com>
2026-04-16 21:19:28 -0600

    I fixed all the issues with the createServer and added the datos.json. At this moment, the /apu/student part doesn't show anything

--- servidor-malo.js
+++ servidor-malo.js
@@ -2,8 +2,11 @@ import http from "http"
 import fs from "fs/promises"
 import path from "path"
 
-const PORT = 3000
+//cambié el puerto porque me generaba algunos errores gracias a otros proyectos que tengo.
+const PORT = 8280 //utilicé este porque estaba disponible.
 
+//Primero que nada añadí un try- catch para recibir todos los errores en la terminal. 
+try {
 const server = http.createServer(async (req, res) => {
   if (req.url === "/") {
     res.writeHead(200, { "Content-Type": "text/plain" })
@@ -16,7 +19,8 @@ const server = http.createServer(async (req, res) => {
     res.end("Ruta de información")
     return
   }
-
+  //Cuarto, cree un archivo llamado "datos.json" para que este no generara errores al no poder encontrarlo. 
+  //este archivo al inicio se encuentra vacío. 
   if (req.url === "/api/student") {
     const filePath = path.join(process.cwd(), "datos.json")
     const texto = fs.readFile(filePath, "utf-8")
@@ -27,8 +31,12 @@ const server = http.createServer(async (req, res) => {
 
   res.writeHead(200, { "Content-Type": "text/plain" })
   res.end("Ruta no encontrada")
-}
-
+})
+//Segundo, cerré esta función para que únicamente haga la parte de crear el server, antes juntaba esto con la parte del listener 
+//Tercero, cerré este listen para que funcionara adecuadamente, además, cambié el mensaje para que indique el puerto correcto. 
 server.listen(PORT, () => {
-  console.log("Servidor corriendo en http://localhost:3000")
+  console.log("Servidor corriendo en http://localhost:8280")
+})
+} catch (error) {
+  
 }
